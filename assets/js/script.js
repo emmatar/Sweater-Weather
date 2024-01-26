55e4eaba164b4ff137a09bb671f2566a044b2681
Elijah Matar <m.matar515@gmail.com>
2024-01-26 11:39:01 -0600

    finished html structure. script file started

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+function init() {
+    const $searchCont = $("#searchContainer");
+    const $searchInput = $("#searchInput");
+    const $buttonCont = $("#buttonContainer");
+    const $buttonInput = $("button");
+    const $historyCont = $("#historyCont");
+    var $historyListItem = $("historyListItem");
+    // const to
+}
