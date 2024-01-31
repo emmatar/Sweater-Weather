9c700950045e0689adea1afc9388b5f63592b57f
Elijah Matar <m.matar515@gmail.com>
2024-01-31 16:41:07 -0600

    displayed 5 day forecast on user input

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,7 +14,7 @@ const weatherIcon = document.querySelector("#weatherIcon");
 const temperature = document.querySelector("#temperature");
 const windSpeed = document.querySelector("#windSpeed");
 const humidity = document.querySelector("#humidity");
-const time = document.querySelector("#time")
+const date = document.querySelector("#date")
 
 
 
@@ -43,31 +43,73 @@ function fetchCityCoord (city) {
 }
 
 function getFutureWeather (lat, lon) {
-    const weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+    const weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
 
     fetch(weatherUrl).then(function (response) {
         if (response.ok) {
 
             response.json().then(function (data) {
                 // pass forloop into display function
-                for (let index = 7; index < data.list.length; index += 8) {
-                    const element = data.list[index];
-                    console.log(index, element);
-                }
+                displayFutureWeather(data) 
             })
         }
     })
 }   
+
+function displayFutureWeather (data) {
+
+
+    for (let index = 7; index < data.list.length; index += 8) {
+        var forecast = document.querySelector("#forecast")
+        var forecastBlock = document.createElement("div")
+        forecastBlock.setAttribute('class', 'col-md-2 dark-blue')
+        // var forecastDate = document.querySelector("#forecastDate")
+        // var descriptionIcon = document.querySelector("#description-icon")
+        // var tempWindHumd = document.querySelector("#temp-wind-humd");
+        var weekDate = document.createElement("h4")
+        var weekWeatherDescription = document.createElement("h4")
+        var weekWeatherIcon = document.createElement("img")
+        var weekTemperature = document.createElement("p")
+        var weekWindSpeed = document.createElement("p")
+        var weekHumidity = document.createElement("p")
+
+
+        const weekInfo = data.list[index];
+        var unixTime = weekInfo.dt;
+        var futureDates = unixTime * 1000;
+        var icon = weekInfo.weather[0].icon;
+
+    
+        weekDate.textContent = weekInfo.dt_txt;
+        weekWeatherDescription.textContent = weekInfo.weather[0].description;
+        weekWeatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${icon}.png`)
+        weekTemperature.textContent = `Temperature: ${weekInfo.main.temp}°F`
+        weekWindSpeed.textContent = `Wind Speed: ${weekInfo.wind.speed}mph`;
+        weekHumidity.textContent = `Humidity: ${weekInfo.main.humidity}%`;
+
+        forecastBlock.appendChild(weekDate)
+        forecastBlock.appendChild(weekWeatherIcon)
+        forecastBlock.appendChild(weekWeatherDescription)
+        forecastBlock.appendChild(weekTemperature)
+        forecastBlock.appendChild(weekWindSpeed)
+        forecastBlock.appendChild(weekHumidity)
+
+        forecast.appendChild(forecastBlock)
+
+
+        // forecast.append()
+        console.log(index, weekInfo);
+    }
+}
 
 
 function getTodayWeather (data) {
     var temp = (Math.floor(((data.main.temp) - 273.15) * 1.8 + 32));
     var today = dayjs();
     var icon = data.weather[0].icon;
-    time.textContent = today.format('MMM D, YYYY')
     cityName.textContent = data.name;
+    date.textContent = today.format('MMM D, YYYY')
     weatherDescription.textContent = `${data.weather[0].description}`;
-    console.log(weatherDescription);
     weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${icon}.png`)
     temperature.textContent = `Temperature: ${temp}°F`;
     windSpeed.textContent = `Wind Speed: ${data.wind.speed}mph`;
